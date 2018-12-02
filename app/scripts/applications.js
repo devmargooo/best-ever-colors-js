394564c080885ac68a810dfb232f187bd263bdb6
Margo <luckinamargo@gmail.com>
2018-12-02 15:48:03 +0700

    add 'add color' input

--- app/scripts/applications.js
+++ app/scripts/applications.js
@@ -1,22 +1,34 @@
 (function (){
 
     'use strict';
-
+    let colors;
+    const input = document.querySelector('input');
+    const list = document.querySelector('.list');
+    input.onblur = addColor;
     setTimeout(() => {
         fetch('/api/colors.json')
     .then(function(response) {
         return response.json();
     })
     .then(function(data) {
-        const colors = data;
-        const list = document.querySelector('.list');
+        colors = data.map(item => item.value);
         for (let i = 0; i < colors.length; i ++) {
             const li = document.createElement('li');
-            li.textContent = colors[i].value;
+            li.textContent = colors[i];
             list.appendChild(li);
         }
+
+
     })
     .catch((e) => {}) 
     }, 500)
+
+    function addColor() {
+        const value = input.value;
+        colors.push(value);
+        const li = document.createElement('li');
+        li.textContent = value;
+        list.appendChild(li);
+    }
 
 })();
